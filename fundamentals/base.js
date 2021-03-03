fbade06c5dbacd9d017f547bc5e6940cf841dc81
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-03-03 12:24:50 -0300

    ✏️ modify the code base

--- fundamentals/base.js
+++ fundamentals/base.js
@@ -1,3 +1,12 @@
-const text = `João Gordo não é o que se pode chamar de ativista bonzinho e politicamente correto. Mas acredite: o apresentador mais desbocado do país está preocupado com o futuro do planeta. Aos 43 anos, dois filhos, vegetariano desde 2005, joão constrói uma nova casa em São Paulo que é feita com madeira de demolição e terá aquecedor solar e reservatório de água da chuva. Se reciclagem de lixo já é uma atitude básica nas grandes cidades, ele vai além: na festa de aniversário de um dos dois filhos, a lembrança que os convidados levaram para casa era feita de garrafas pet reutilizadas. E as roupas que o pessoal em casa não usava mais viraram capas de almofadas, feitas por sua mulher, Viviana Torrico. “É muito legal: depois de velho, eu virei hippie”, brinca ele.`;
+const text = `Joooooooooooooooooooãoooooooooooooooooooo Gordo não é o que se pode chamar de ativista bonzinho e politicamente correto. Mas acredite: o apresentador mais desbocado do país está preocupado com o futuro do planeta. Aos 43 anos, dois filhos, vegetariano desde 2005, joão constrói uma nova casa em São Paulo que é feita com madeira de demolição e terá aquecedor solar e reservatório de água da chuva. Se reciclagem de lixo já é uma atitude básica nas grandes cidades, ele vai além: na festa de aniversário de um dos dois filhos, a lembrança que os convidados levaram para casa era feita de garrafas pet reutilizadas. E as roupas que o pessoal em casa não usava mais viraram capas de almofadas, feitas por sua mulher, Viviana Torrico. “É muito legal: depois de velho, eu virei hippie”, brinca ele.`;
 
-export { text };
+const files = [
+  "files files.png",
+  "files files2.jpg",
+  "files sleif.JPG",
+  "files files3.jpeg",
+  "files files4.JPEG",
+  "files files4.JPEeeeeeeeeeeeeeeeEG",
+];
+
+export { text, files };
